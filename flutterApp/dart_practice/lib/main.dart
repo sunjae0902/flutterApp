e45f89e5d81d4e21e9a9fad5c0474c06f9499b12
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-05 17:26:15 +0900

    Update main.dart
    주석 추가

--- flutterApp/dart_practice/lib/main.dart
+++ flutterApp/dart_practice/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() { //main
 
   print("start");
-  var stream = Stream.periodic(Duration(seconds: 1),(x)=> x+1)
+  
+  var stream = Stream.periodic(Duration(seconds: 1),(x)=> x+1) //데이터를 순서대로 주고받을 때 사용하는 스트림. 매초마다 데이터를 1개씩 최대 5개 생성
   .take(5)
-  .listen((x)=> print('periodic: $x'));
+  .listen((x)=> print('periodic: $x')); //동기적 데이터 생성 시 이벤트 처리->출력
 
-   Stream.fromIterable(['one','2.5','three',4,5]).listen((dynamic x)=> print('fromIterable: $x'));
+   Stream.fromIterable(['one','2.5','three',4,5]).listen((dynamic x)=> print('fromIterable: $x'));  //일반적으로 데이터를 처리
 
-   Stream.fromFuture(getData()).listen((x)=> print('fromFuture: $x'));
+   Stream.fromFuture(getData()).listen((x)=> print('fromFuture: $x')); //비동기식 함수 처리. 3초 지연 후 데이터 처리
    print('do something');
 
 }
-Future <String> getData() async{
+
+Future <String> getData() async{ //비동기방식 함수 
   return Future.delayed(Duration(seconds: 3),()=> 'after 3 seconds');
 }
 

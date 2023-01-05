9718ac370e05795991f8630f3a1a58000f5e20be
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-06 01:44:49 +0900

    Update main.dart

--- flutterApp/fileio_example/lib/main.dart
+++ flutterApp/fileio_example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FileApp(), //파일 호
+      home: FileApp(), // 호출
     );
   }
 }

2731ba55d1daca033a06259b787352330d2e7c8a
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-06 02:18:14 +0900

    Update secondDetail.dart

--- flutterApp/todo_list/lib/secondDetail.dart
+++ flutterApp/todo_list/lib/secondDetail.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //SecondDetail 클래스 정의
 
 class SecondDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    TextEditingController controller=new TextEditingController();
+    TextEditingController controller=new TextEditingController(); //사용자가 할 일을 추가할 수 있게 하는 텍스트 필드 생성
     return Scaffold(
       appBar: AppBar(
         title: Text('Second Page'),
@@ -18,7 +18,7 @@ class SecondDetail extends StatelessWidget{
               ),
               ElevatedButton(
                   onPressed: (){
-                    Navigator.of(context).pop(controller.value.text);
+                    Navigator.of(context).pop(controller.value.text); // 버튼을 눌렀을 때 pop하여 1페이지로 이동함
                   },
                   child: Text('저장하기'),),
             ],

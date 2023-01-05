76c6be51d994513ed7c868da526ea76ee670cdbd
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-06 02:15:27 +0900

    Update subDetail.dart

--- flutterApp/todo_list/lib/subDetail.dart
+++ flutterApp/todo_list/lib/subDetail.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // 첫번째 화면을 표시하는 _SubDetail 클래스 정의, 페이지 이동 함수 구현
 
 class SubDetail extends StatefulWidget{
   @override
@@ -6,7 +6,7 @@ class SubDetail extends StatefulWidget{
 }
 
 class _SubDetail extends State<SubDetail>{
-  List<String> todoList=new List.empty(growable: true);
+  List<String> todoList=new List.empty(growable: true); //할 일을 추가할 수 있는 리스트, 첫번째 화면에서 아래 목록을 보여줌
   @override
   void initState(){
     super.initState();
@@ -20,7 +20,7 @@ class _SubDetail extends State<SubDetail>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sub Detail Example'),
+        title: Text('Sub Detail Example'),  //1페이지 앱바 텍스트
       ),
       body: ListView.builder(
         itemBuilder: (context,index){
@@ -31,12 +31,12 @@ class _SubDetail extends State<SubDetail>{
                 style: TextStyle(fontSize: 30),
               ),
               onTap: (){
-                Navigator.of(context).pushNamed('/third',arguments: todoList[index]);
+                Navigator.of(context).pushNamed('/third',arguments: todoList[index]); //3페이지로 
               },
             ),
           );
         },
-        itemCount: todoList.length,
+        itemCount: todoList.length, //할 일 리스트의 길이 (수)
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -48,9 +48,9 @@ class _SubDetail extends State<SubDetail>{
     );
   }
   void _addNavigation(BuildContext context) async{
-    final result=await Navigator.of(context).pushNamed('/second'); //두번째 페이지에서 새로 추가한 데이터가 result변수에 저장됨
+    final result=await Navigator.of(context).pushNamed('/second'); //두번째 페이지에서 새로 추가한 데이터가 result변수에 저장되고, 라우팅 기능으로 페이지를 이동함
     setState(() {
-      todoList.add(result as String); //문자열로 형변환
+      todoList.add(result as String); //문자열로 형변환하여 리스트에 추가
     });
   }
 }

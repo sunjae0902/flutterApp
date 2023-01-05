5feb9f44ff23a51b0554ca4868437edb608f88ad
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-05 20:37:14 +0900

    Update main.dart

--- flutterApp/internal_example/lib/main.dart
+++ flutterApp/internal_example/lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'; //패키지 임포트
 
 
 void main() {
@@ -10,39 +10,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'), //첫 화면 앱 제목
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,22 +33,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 0; 
 
-  void _setData(int value) async{ //데이터를 저장하는 비동기 함수
+  void _setData(int value) async{ //_MyHomePageState 클래스에 카운트를 저장하는 비동기 함수
     var key='count';
-    SharedPreferences pref=await SharedPreferences.getInstance();
-    pref.setInt(key, value);
+    SharedPreferences pref=await SharedPreferences.getInstance();  //해당 클래스의 객체 pref를 생성한 후 setInt 함수 호출-> 공유 환경설정 파일에 데이터 저장
+    pref.setInt(key, value); //데이터 저장(count, 키-값 쌍으로)
   }
 
-  void _loadData() async{
+  void _loadData() async{ //카운트 값 (공유 환경설정 파일에 저장된 값)을 가져오는 함수
     var key= 'count';
-    SharedPreferences pref=await SharedPreferences.getInstance();
+    SharedPreferences pref=await SharedPreferences.getInstance(); //객체 생성 후 가져옴
     setState(() {
       var value=pref.getInt(key);
-      if (value==null) {
+      if (value==null) { //값이 없을 경우
         _counter = 0;
-      } else {
+      } else { //받은 경우 _counter 변수에 저장
         _counter=value;
 
       }
@@ -79,31 +61,23 @@ class _MyHomePageState extends State<MyHomePage> {
     _loadData();
   }
 
-  void _incrementCounter() { //버튼을 누를 때마다 호출되는 함수
+  void _incrementCounter() { //버튼을 누를 때마다 호출되는 함수-> _counter 1증가-> 데이터 저장 후 
     setState(() {
-
       _counter++;
-      _setData(_counter);
+      _setData(_counter); 
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+       
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -121,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }

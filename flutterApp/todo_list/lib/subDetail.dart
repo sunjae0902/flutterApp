0a7ab8840f8d3b12745b095ffdbfd2efdee102c2
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-06 02:34:48 +0900

    Update subDetail.dart

--- flutterApp/todo_list/lib/subDetail.dart
+++ flutterApp/todo_list/lib/subDetail.dart
@@ -22,7 +22,7 @@ class _SubDetail extends State<SubDetail>{
       appBar: AppBar(
         title: Text('Sub Detail Example'),  //1페이지 앱바 텍스트
       ),
-      body: ListView.builder(
+      body: ListView.builder( //리스트뷰에 할 일을 보여주는 기능 구현
         itemBuilder: (context,index){
           return Card(
             child: InkWell(
@@ -31,7 +31,7 @@ class _SubDetail extends State<SubDetail>{
                 style: TextStyle(fontSize: 30),
               ),
               onTap: (){
-                Navigator.of(context).pushNamed('/third',arguments: todoList[index]); //3페이지로 
+                Navigator.of(context).pushNamed('/third',arguments: todoList[index]); //탭 시 3페이지로 이동 (할 일 보여주기) , 사용자가 선택한 할 일을 두번째 인자 argumentes로 전달함
               },
             ),
           );
@@ -41,14 +41,14 @@ class _SubDetail extends State<SubDetail>{
 
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          _addNavigation(context);
+          _addNavigation(context); //버튼 누를 시 새 할 일을 추가할 수 있는 기능 구현 
           },
         child: Icon(Icons.add),
       ),
     );
   }
   void _addNavigation(BuildContext context) async{
-    final result=await Navigator.of(context).pushNamed('/second'); //두번째 페이지에서 새로 추가한 데이터가 result변수에 저장되고, 라우팅 기능으로 페이지를 이동함
+    final result=await Navigator.of(context).pushNamed('/second'); //두번째 페이지에서 새로 추가한 데이터가 result변수에 저장되고, 라우팅 기능으로 2페이지 이동함
     setState(() {
       todoList.add(result as String); //문자열로 형변환하여 리스트에 추가
     });

984f6b9ea97fbb8654973c63957710c7c7c6bba6
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-05 20:05:01 +0900

    Update main.dart

--- flutterApp/largefile_download/lib/main.dart
+++ flutterApp/largefile_download/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'largeFileMain.dart';
+import 'largeFileMain.dart'; //클래스가 정의된 파일 임포트 
 void main() {
   runApp( MyApp());
 }
@@ -7,24 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: LargeFileMain(),
+      
+       
+      home: LargeFileMain(), //이미지 파일을 내려받는 클래스 
     );
   }
 }

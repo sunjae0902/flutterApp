0d911eb69a1f67909d92dfee7419823582287ed3
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2023-01-06 02:27:58 +0900

    Update thirdPage.dart

--- flutterApp/todo_list/lib/thirdPage.dart
+++ flutterApp/todo_list/lib/thirdPage.dart
@@ -1,13 +1,11 @@
-
-
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //마지막 페이지
 
 class ThirdDetail extends StatelessWidget{
   @override
 
 
   Widget build(BuildContext context){
-    final String args = ModalRoute.of(context)!.settings.arguments.toString();
+    final String args = ModalRoute.of(context)!.settings.arguments.toString(); // 라우터로 전달받은 데이터를 문자열로 변환하여 가져옴. 2페이지에서 보낸 argeument값을 입력하고 텍스트 위젯에 출력한다.
     return Scaffold(
       appBar: AppBar(
         title: Text('Third Page'),
@@ -17,10 +15,10 @@ class ThirdDetail extends StatelessWidget{
         child: Center(
           child: ElevatedButton(
             onPressed: (){
-              Navigator.of(context).pop();
+              Navigator.of(context).pop(); //스택에서 세번째 페이지가 제거되고, SubDetail 페이지로 돌아옴.
             },
             child: Text(args,
-              style: TextStyle(fontSize: 30)) ,
+              style: TextStyle(fontSize: 30)) , //가져온 텍스트 표시 (할 일 리스트 화면에서 특정 할 일을 선택하면 3페이지 이동하여 그 할 일의 텍스트를 화면 중간에 보여줌)
           ),
         ),
       ),
